992fa989f46f4ab86d78f494362062082d9d91ee
marira <rongrazzo@gmail.com>
2021-05-01 20:48:43 +0600

    hover added

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
+let L = 240;
+let F = 10;
+let opacityFactor = F / (2 * L);
+let opacity = 0.3;
+
 window.addEventListener("DOMContentLoaded", (event) => {
-  for (let i = 240; i > 0; i -= 10) {
-    createRect(i);
+  for (let i = L; i >= 0; i -= F) {
+    opacity += opacityFactor;
+    createRect(i, opacity);
   }
 });
 
-function createRect(w) {
-  console.log(w);
+function createRect(w, opacity) {
+  console.log(opacity);
   var node = document.createElement("div");
   node.className = "rect";
   node.style.width = w + "px";
   node.style.height = w + "px";
+  node.style.opacity = opacity;
   node.style.transform = "rotate(" + w + "deg)";
   node.style["margin-left"] = "-" + w / 2 + "px";
   node.style["margin-top"] = "-" + w / 2 + "px";
-  console.log(node);
-
   document.getElementById("all-rect").appendChild(node);
 }
